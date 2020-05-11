aea68055d3d5477370188d9ca72d9200f5e63b03
Kriss <geraltrivia19@gmail.com>
2020-05-11 09:58:08 +0200

    Extras 2

--- PracticePython/Exe3.py
+++ PracticePython/Exe3.py
@@ -3,9 +3,11 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 for i in a:
     if i != 5:
         print(i)
-# Extras 1
+# # Extras 1
 b = []
 for i in a:
     if i < 5:
         b.append(i)
 print(b)
+# Extras 2
+print([i for i in a if i < 5])
