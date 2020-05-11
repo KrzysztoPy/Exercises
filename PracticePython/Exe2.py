49ac0986270f07e3453b74edc849ee52f6fa8b1f
Kriss <geraltrivia19@gmail.com>
2020-05-11 09:46:08 +0200

    Extras 2:

--- PracticePython/Exe2.py
+++ PracticePython/Exe2.py
@@ -1,18 +1,34 @@
+# while True:
+#     try:
+#         input_num = int(input("Give me a number: "))
+#         break
+#     except ValueError:
+#         print("You must give a number. Try again.")
+#
+# if input_num % 2 == 0:
+#     # Extras 1:
+#     if input_num % 4 == 0:
+#         print("Number is a multiple of 4.")
+#     else:
+#         print("Number is even")
+# else:
+#     print("Number is odd")
+
+# Extras 2
+
 while True:
     try:
-        input_num = int(input("Give me a number: "))
+        num = int(input("Give me a number: "))
         break
     except ValueError:
         print("You must give a number. Try again.")
-
-if input_num % 2 == 0:
-    # Extras 1:
-    if input_num % 4 == 0:
-        print("Number is a multiple of 4.")
-    else:
-        print("Number is even")
+while True:
+    try:
+        check = int(input("Give me a number: "))
+        break
+    except ValueError:
+        print("You must give a number. Try again.")
+if num % check == 0:
+    print("The number is divided evenly.")
 else:
-    print("Number is odd")
-
-
-
+    print("The number isn't divided evenly.")
