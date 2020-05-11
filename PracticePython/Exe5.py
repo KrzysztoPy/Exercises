476e983f0f7457f1d0b650e8668c51bcc9babee2
Kriss <geraltrivia19@gmail.com>
2020-05-11 10:51:16 +0200

    Extras 1

--- PracticePython/Exe5.py
+++ PracticePython/Exe5.py
@@ -1,7 +1,25 @@
+from random import randint
+
 b = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-if a.__len__() > b.__len__():
-    print([i for i in a if i in b])
-else:
-    print([i for i in b if i in a])
+
+def common():
+    print("List a: {} \nList b: {}\n".format(a, b))
+
+    if a.__len__() > b.__len__():
+        print("Common between a and b: {}".format([i for i in a if i in b]), "\n")
+    elif a.__len__() < b.__len__():
+        print("Common between a and b: {}".format([i for i in b if i in a]), "\n")
+    elif a.__len__() == b.__len__():
+        print("Common between a and b: {}".format([i for i in a if i in b]), "\n")
+
+
+common()
+
+# Extras 1
+for i in range(0, randint(0, 15)):
+    a.append(randint(0, 101))
+for i in range(0, randint(0, 15)):
+    b.append(randint(0, 101))
+common()
