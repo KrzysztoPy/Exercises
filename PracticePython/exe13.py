31f5b806a6f889abd269609d44313241f5be7552
Kriss <geraltrivia19@gmail.com>
2020-05-12 12:52:27 +0200

    Exe 13

---
+++ PracticePython/exe13.py
@@ -0,0 +1,29 @@
+# fibb 0 1 1 2 3 5 8 13
+
+def max_range():
+    while True:
+        try:
+            return int(input("Give me a number: "))
+        except ValueError:
+            print("Wrong input data. Try again.")
+
+
+def fibb(num = max_range()):
+    first_num = 0
+    second_num = 1
+    print(first_num)
+    print(second_num)
+    while True:
+        tmp = second_num
+        second_num = first_num + second_num
+        first_num = tmp
+        print(second_num)
+        if (second_num > num):
+            break
+
+
+def menu():
+    fibb()
+
+
+menu()
