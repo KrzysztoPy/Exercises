debe2afa833914e60c14a28303a7d406b7708af4
Kriss <geraltrivia19@gmail.com>
2020-05-20 16:58:22 +0200

    Exe 20: Test for binary search

--- PracticePython/Exe20.py
+++ PracticePython/Exe20.py
@@ -1,12 +1,13 @@
 from random import randint
+import math
 
-list = set()
+sorted_list = set()
 
 
 def gen_list():
     for i in range(0, randint(0, 100)):
         list.add(randint(0, 100))
-    sorted(list)
+    sorted(sorted_list)
 
 
 def normal_search(collection, value):
@@ -15,6 +16,39 @@ def normal_search(collection, value):
     return print("It is not in collection")
 
 
-gen_list()
-normal_search(list, 1)
-print(list)
+# 1 2 3 4 5 6 7 8 9
+# 1 2 3 4 5 6 7 8
+# Extras:
+def binary_search(collection=set(), value=0):
+    while True:
+        if len(collection) > 1:
+            if collection[math.ceil((len(collection)) / 2) - 1] > value:
+                for i in range(len(collection) - 1, (math.ceil((len(collection)) / 2)) - 2, -1):
+                    collection.remove(collection[i])
+            elif collection[math.ceil((len(collection)) / 2) - 1] < value:
+                for i in range(0, (math.ceil((len(collection)) / 2)), 1):
+                    collection.remove(collection[0])
+            elif collection[math.ceil((len(collection)) / 2) - 1] == value:
+                return print("It is in collection")
+        else:
+            if collection[0] == value:
+                return print("It is in collection")
+            else:
+                return print("It is not in collection")
+
+
+def test():
+    test = [1, 2, 3, 4, 5, 6, 7, 8]
+    test1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for i in range(1, 10, 1):
+        print("Whether {} is in list?".format(i))
+        binary_search(test.copy(), i)
+        print("Whether {} is in list?".format(i))
+        binary_search(test1.copy(), i)
+
+
+test()
+# gen_list()
+# normal_search(sorted_list, 1)
+# binary_search
+# print(sorted_list)
