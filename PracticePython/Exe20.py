3f0ad46c6d0495c6dcdd4d058325aef59a57d4f8
Kriss <geraltrivia19@gmail.com>
2020-05-20 17:34:36 +0200

    Exe 20: Test for binary search + Extras

--- PracticePython/Exe20.py
+++ PracticePython/Exe20.py
@@ -1,13 +1,28 @@
 from random import randint
 import math
 
-sorted_list = set()
+
+def get_num():
+    while True:
+        try:
+            num = int(input("Give me a number from 0 to 100: "))
+            if num < 0:
+                print("Number must be greater than 0 ")
+                continue
+            elif num > 100:
+                print("Number must be less than 100 ")
+                continue
+            return num
+        except ValueError:
+            print("You can only enter a number from range 0 to 100 ")
 
 
 def gen_list():
-    for i in range(0, randint(0, 100)):
-        list.add(randint(0, 100))
-    sorted(sorted_list)
+    sorted_list = set()
+    for i in range(0, randint(1, 100)):
+        sorted_list.add(randint(0, 100))
+    print(sorted(sorted_list))
+    return list(sorted(sorted_list.copy()))
 
 
 def normal_search(collection, value):
@@ -16,10 +31,8 @@ def normal_search(collection, value):
     return print("It is not in collection")
 
 
-# 1 2 3 4 5 6 7 8 9
-# 1 2 3 4 5 6 7 8
 # Extras:
-def binary_search(collection=set(), value=0):
+def binary_search(collection=[], value=0):
     while True:
         if len(collection) > 1:
             if collection[math.ceil((len(collection)) / 2) - 1] > value:
@@ -32,23 +45,25 @@ def binary_search(collection=set(), value=0):
                 return print("It is in collection")
         else:
             if collection[0] == value:
+                print(collection)
                 return print("It is in collection")
             else:
+                print(collection)
                 return print("It is not in collection")
 
 
-def test():
-    test_list0 = [1, 2, 3, 4, 5, 6, 7, 8]
-    test_list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    for i in range(1, 10, 1):
-        print("Whether {} is in list?".format(i))
-        binary_search(test_list0.copy(), i)
-        print("Whether {} is in list?".format(i))
-        binary_search(test_list1.copy(), i)
+# def test():
+#     test_list0 = [1, 2, 3, 4, 5, 6, 7, 8]
+#     test_list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#     for i in range(1, 10, 1):
+#         print("Whether {} is in list?".format(i))
+#         binary_search(test_list0.copy(), i)
+#         print("Whether {} is in list?".format(i))
+#         binary_search(test_list1.copy(), i)
 
 
-test()
-# gen_list()
-# normal_search(sorted_list, 1)
-# binary_search
+# test()
+list_gen = gen_list()
+normal_search(list_gen, get_num())
+binary_search(list_gen, get_num())
 # print(sorted_list)
